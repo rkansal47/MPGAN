ceadfdf70e1eea115f74dc355ca12d9667b70d3a
rkansal47 <raghavk1997@yahoo.co.in>
2021-09-18 09:47:13 -0700

    restructuring and w1 methods

--- jetnet/evaluation/particlenet.py
+++ jetnet/evaluation/particlenet.py
@@ -1,5 +1,3 @@
-import logging
-
 import torch
 from torch import nn
 
@@ -8,6 +6,7 @@ from torch_cluster import knn_graph
 
 import numpy as np
 import torch.nn.functional as F
+
 
 class ParticleNetEdgeNet(nn.Module):
     def __init__(self, in_size, layer_size):
@@ -30,11 +29,11 @@ class ParticleNetEdgeNet(nn.Module):
         return self.model(x)
 
     def __repr__(self):
-        return '{}(nn={})'.format(self.__class__.__name__, self.model)
+        return "{}(nn={})".format(self.__class__.__name__, self.model)
 
 
 class ParticleNet(nn.Module):
-    def __init__(self, num_hits, node_feat_size, num_classes=5, device=torch.device('cpu')):
+    def __init__(self, num_hits, node_feat_size, num_classes=5, device=torch.device("cpu")):
         super(ParticleNet, self).__init__()
         self.num_hits = num_hits
         self.node_feat_size = node_feat_size
@@ -54,30 +53,18 @@ class ParticleNet(nn.Module):
         self.output_sizes = np.cumsum(self.kernel_sizes)
 
         self.edge_nets.append(ParticleNetEdgeNet(self.node_feat_size, self.kernel_sizes[1]))
-        self.edge_convs.append(EdgeConv(self.edge_nets[-1], aggr='mean'))
+        self.edge_convs.append(EdgeConv(self.edge_nets[-1], aggr="mean"))
 
         for i in range(1, self.num_edge_convs):
-            self.edge_nets.append(ParticleNetEdgeNet(self.output_sizes[i], self.kernel_sizes[i + 1]))  # adding kernel sizes because of skip connections
-            self.edge_convs.append(EdgeConv(self.edge_nets[-1], aggr='mean'))
+            # adding kernel sizes because of skip connections
+            self.edge_nets.append(ParticleNetEdgeNet(self.output_sizes[i], self.kernel_sizes[i + 1]))
+            self.edge_convs.append(EdgeConv(self.edge_nets[-1], aggr="mean"))
 
         self.fc1 = nn.Sequential(nn.Linear(self.output_sizes[-1], self.fc_size))
 
         self.dropout_layer = nn.Dropout(p=self.dropout)
 
         self.fc2 = nn.Linear(self.fc_size, self.num_classes)
-
-        # logging.info("edge nets: ")
-        # logging.info(self.edge_nets)
-
-        logging.info("edge_convs: ")
-        logging.info(self.edge_convs)
-
-        logging.info("fc1: ")
-        logging.info(self.fc1)
-
-        logging.info("fc2: ")
-        logging.info(self.fc2)
-
 
     def forward(self, x, ret_activations=False, relu_activations=False):
         batch_size = x.size(0)
@@ -87,15 +74,23 @@ class ParticleNet(nn.Module):
         batch = torch.cumsum(zeros, 0) - 1
 
         for i in range(self.num_edge_convs):
-            edge_index = knn_graph(x[:, :2], self.k, batch) if i == 0 else knn_graph(x, self.k, batch)  # using only angular coords for knn in first edgeconv block
+            # using only angular coords for knn in first edgeconv block
+            edge_index = knn_graph(x[:, :2], self.k, batch) if i == 0 else knn_graph(x, self.k, batch)
             x = torch.cat((self.edge_convs[i](x, edge_index), x), dim=1)  # concatenating with original features i.e. skip connection
 
         x = global_mean_pool(x, batch)
         x = self.fc1(x)
 
         if ret_activations:
-            if relu_activations: return F.relu(x)
-            else: return x    # for Frechet ParticleNet Distance
-        else: x = self.dropout_layer(F.relu(x))
+            if relu_activations:
+                return F.relu(x)
+            else:
+                return x  # for Frechet ParticleNet Distance
+        else:
+            x = self.dropout_layer(F.relu(x))
 
         return self.fc2(x)  # no softmax because pytorch cross entropy loss includes softmax
+
+    # TODO: !!
+    # def __repr__(self):
+    #     return ""
