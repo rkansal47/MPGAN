889d793d43dc31a6cd3254f60aeda357dd57929b
rkansal47 <raghavk1997@yahoo.co.in>
2021-10-01 16:12:26 -0700

    default lrs

--- setup_training.py
+++ setup_training.py
@@ -226,8 +226,8 @@ def parse_args():
     parser.add_argument("--optimizer", type=str, default="rmsprop", help="pick optimizer", choices=["adam", "rmsprop", "adadelta", "agcd"])
     parser.add_argument("--loss", type=str, default="ls", help="loss to use - options are og, ls, w, hinge", choices=["og", "ls", "w", "hinge"])
 
-    parser.add_argument("--lr-disc", type=float, default=3e-5, help="learning rate discriminator")
-    parser.add_argument("--lr-gen", type=float, default=1e-5, help="learning rate generator")
+    parser.add_argument("--lr-disc", type=float, default=0, help="learning rate for discriminator; defaults are 3e-5, 6e-5, and 1.5e-5 for gluon, top, and quark jet resp.")
+    parser.add_argument("--lr-gen", type=float, default=0, help="learning rate for generator; defaults are 1e-5, 2e-5, and 0.5e-5 for gluon, top, and quark jet resp.")
     parser.add_argument("--beta1", type=float, default=0.9, help="Adam optimizer beta1")
     parser.add_argument("--beta2", type=float, default=0.999, help="Adam optimizer beta2")
     parser.add_argument("--batch-size", type=int, default=0, help="batch size")
@@ -413,6 +413,22 @@ def process_args(args):
                             args.batch_size = 100
                         else:
                             args.batch_size = 32
+
+    if args.lr_disc == 0:
+        if args.jets == 'g':
+            args.lr_disc = 3e-5
+        elif args.jets == 't':
+            args.lr_disc = 6e-5
+        elif args.jets == 'q':
+            args.lr_disc = 1.5e-5
+
+    if args.lr_gen == 0:
+        if args.jets == 'g':
+            args.lr_gen = 1e-5
+        elif args.jets == 't':
+            args.lr_gen = 2e-5
+        elif args.jets == 'q':
+            args.lr_gen = 0.5e-5
 
     if args.n:
         if not (args.no_save_zero_or or args.num_hits == 100):
