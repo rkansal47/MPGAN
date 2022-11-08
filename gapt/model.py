caead1bc1a399d2f030ba0380b52dec925a4d247
AnniLi1212 <anni314159265@gmail.com>
2022-11-07 20:35:30 -0800

    mask

--- gapt/model.py
+++ gapt/model.py
@@ -183,10 +183,10 @@ class ISAB(nn.Module):
         self.mab0 = MAB(embed_dim=embed_dim, **mab_args)
         self.mab1 = MAB(embed_dim=embed_dim, **mab_args)
 
-    def forward(self, X, mask: Tensor = None):
+    def forward(self, X, num_inds, mask: Tensor = None):
         H = self.mab0(self.I.repeat(X.size(0), 1, 1), X)
         if mask is not None:
-            mask = mask.transpose(-2, -1).repeat((1, mask.shape[-2], 1))
+            mask = mask.transpose(-2, -1).repeat((1, mask.shape[-2], num_inds))
         return self.mab1(X, H, mask)
 
 
